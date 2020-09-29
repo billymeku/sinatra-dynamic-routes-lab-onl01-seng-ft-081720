ec1ec76d4005cbf5631f9bed7e95c16a21265d44
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-29 06:51:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,9 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do
-    params[:number].to_i.times 
-    "#{params[:phrase]}"
+    @number = params[:number].to_i.times 
+    @phrase = params[:phrase]
+    
   end 
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
