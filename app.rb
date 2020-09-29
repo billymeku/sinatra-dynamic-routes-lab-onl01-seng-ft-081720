cf0d4bac1cc71b8fb907860d76fc8c38b5d52d73
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-29 06:47:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,10 +31,6 @@ class App < Sinatra::Base
        @number1 = params[:number1].to_i
        @number2 = params[:number2].to_i   
        "#{@number1 + @number2} #{@number1 - @number2} #{@number1 * @number2} #{@number1 / @number2}" 
-    
-      
-     
-  
   end 
 
 end
