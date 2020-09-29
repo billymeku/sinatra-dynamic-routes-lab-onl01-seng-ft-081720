6b250e7e60a635481ecbae29c140f743ad00a30d
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-29 06:44:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,10 +30,10 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do 
        @number1 = params[:number1].to_i
        @number2 = params[:number2].to_i   
-       "#{@number1 + @number2} #{@number1 - @number2}"
+       "#{@number1 + @number2} #{@number1 - @number2} #{@number1 * @number2} #{@number1 / @number2}" 
     
-      "#{@number1 * @number2}"
-      "#{@number1 / @number2}"
+      
+     
   
   end 
 
